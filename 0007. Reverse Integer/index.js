631c6969c3f1ae8db3d1f35061fee055fc21a8f2
Hsuan Chang <hsuanchang@starlux-airlines.com>
2020-06-24 17:01:27 +0800

    0007 ans not test file

---
+++ 0007. Reverse Integer/index.js
@@ -0,0 +1,19 @@
+const reverse = (x) => {
+  let xArray = Array.from(x.toString());
+  xArray.reverse();
+  let returnNum = 0;
+  if (xArray[xArray.length - 1] == '-') {
+    xArray[xArray.length - 1] = null;
+    let tmp = xArray.join('');
+    returnNum = tmp * -1;
+  } else {
+    returnNum = xArray.join('');
+  }
+  if (Math.pow(2, 31) < parseInt(returnNum) || Math.pow(2, 31) * -1 > parseInt(returnNum)) {
+    return 0;
+  } else {
+    return returnNum;
+  }
+};
+
+export default reverse;
